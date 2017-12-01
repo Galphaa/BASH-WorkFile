ae5f1c7586e29da34e66f0462f2e64b2ba66d62f
Giorgi Keratishvili <13200109@ibsu.edu.ge>
2017-12-01 14:30:57 +0400

    Update 34 - Remote accses.sh

--- 34 - Remote accses.sh
+++ 34 - Remote accses.sh
@@ -1,3 +1,7 @@
+## bash -s is for making  Display Readline key sequences bound to macros and the strings they output in such a way that they can be used as input or in a Readline initialization file.
+## they are used to make making redireqcting of output for your log in order not to creat
+
+
 #!/bin/bash
 server=$1
 
